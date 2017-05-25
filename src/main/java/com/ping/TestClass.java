3b1db2a6b99a13b99f58a5e8814f60c4e69f45f9
zhangxiaoping <xiaoping6783@163.com>
2017-05-25 23:51:24 +0800

    first java class

--- src/main/java/com/ping/TestClass.java
+++ src/main/java/com/ping/TestClass.java
@@ -4,4 +4,8 @@ package com.ping;
  * Created by zhangxiaoping on 17/5/25.
  */
 public class TestClass {
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
 }
